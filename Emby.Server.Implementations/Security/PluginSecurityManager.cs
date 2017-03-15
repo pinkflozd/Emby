aaac50f8405100fde19e2418092bbd1249ddff57
Luka Karinja <luka.karinja@gmail.com>
2017-03-15 10:21:00 +0100

    hack3

--- Emby.Server.Implementations/Security/PluginSecurityManager.cs
+++ Emby.Server.Implementations/Security/PluginSecurityManager.cs
@@ -269,35 +269,14 @@ namespace Emby.Server.Implementations.Security
 
             if (!(lastChecked > DateTime.UtcNow.AddDays(-1)) || !reg.registered)
             {
-                var data = new Dictionary<string, string>
-                {
-                    { "feature", feature }, 
-                    { "key", SupporterKey }, 
-                    { "mac", _appHost.SystemId }, 
-                    { "systemid", _appHost.SystemId }, 
-                    { "mb2equiv", mb2Equivalent }, 
-                    { "ver", version }, 
-                    { "platform", _appHost.OperatingSystemDisplayName }
-                };
 
                 try
                 {
-                    var options = new HttpRequestOptions
-                    {
-                        Url = MBValidateUrl,
-
-                        // Seeing block length errors
-                        EnableHttpCompression = false,
-                        BufferContent = false
-                    };
-
-                    options.SetPostData(data);
-
-                    using (var json = (await _httpClient.Post(options).ConfigureAwait(false)).Content)
-                    {
-                        reg = _jsonSerializer.DeserializeFromStream<RegRecord>(json);
-                        success = true;
-                    }
+                    reg.featId = feature;
+                    reg.registered = true;
+                    reg.expDate = DateTime.UtcNow.AddDays(365);
+                    reg.key = feature;
+                    success = true;
 
                     if (reg.registered)
                     {
